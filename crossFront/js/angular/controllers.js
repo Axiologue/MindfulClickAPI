87dfdc35ffe5eda0b1785514588b33af2b78fdae
Matt Broach <go.for.dover@gmail.com>
2015-07-24 14:05:43 -0400

    refactored to using ng-includes instead of repeated html

--- crossFront/js/angular/controllers.js
+++ crossFront/js/angular/controllers.js
@@ -27,27 +27,20 @@ crossControllers.controller('SingleArticleCtrl',['$scope', 'Article','Cross',fun
       elements.splice(index,1);
   }
 
-  $scope.edit = false;
-  $scope.analysis = false;
+  $scope.crossForm = 'templates/includes/cross_form.html';
+  $scope.articleTemplate = 'templates/includes/article_base.html';
+  $scope.analysis = {analysis:false};
   $scope.error = {
     error: false,
     msg: ""
   };
-
-  $scope.newCross = {
-    company: "",
-    subcategory: "",
-    notes: "",
-    score: "",
-    article: $scope.article.id
-  }
-
+  $scope.cancel = true;
 
   $scope.articleSubmit = function () {
     $scope.tempArticle.csrfmiddlewaretoken = $scope.csrftoken;
 
     Article.update({articleID: $scope.article.id},$scope.tempArticle,function () {
-      $scope.edit = false;
+      $scope.articleTemplate = 'templates/includes/article_base.html';
       $scope.article = $.extend({},$scope.tempArticle);
     },function (response) {
       $scope.error.msg = JSON.stringify(response.data);
@@ -57,7 +50,11 @@ crossControllers.controller('SingleArticleCtrl',['$scope', 'Article','Cross',fun
 
   $scope.readyForm = function() {
     $scope.tempArticle = $.extend({},$scope.article);
-    $scope.edit = !$scope.edit;
+    $scope.articleTemplate = 'templates/includes/article_form.html';
+  }
+
+  $scope.flipBack = function () {
+    $scope.articleTemplate='templates/includes/article_base.html';
   }
 
   $scope.articleDelete = function () {
@@ -77,44 +74,20 @@ crossControllers.controller('SingleArticleCtrl',['$scope', 'Article','Cross',fun
     });
   }
 
-  $scope.newCrossSubmit = function () {
-    $scope.newCross.csrfmiddlewaretoken = $scope.csrftoken;
-
-    Cross.save({crossID: 'new'},$scope.newCross,function (value,response) {
-      $scope.article.data = $scope.article.data || [];
-
-      // Replace element IDs with actual names
-      value.company = $.grep($scope.companies,function(v) {return v.id === value.company})[0].name;
-      value.subcategory = $.grep($scope.categories,function(v) {return v.id === value.subcategory})[0].name;
-
-      $scope.article.data.push(value);
-      $scope.analysis = false;
-      $scope.error.error = false;
-
-      //if the article is in the Unanalyzed list, move it to the analyzed list
-      if($scope.article.data.length === 1) {
-        $scope.crossList.push($scope.article);
-        removeArticle($scope.articles);
-      }
-    },function (response) {
-      $scope.error.msg = JSON.stringify(response.data);
-      $scope.error.error = true;
-    });
-
-    
-  }
+  
 
   
 
 }]);
 
 crossControllers.controller('NewArticleCtrl',['$scope','Article',function ($scope, Article) {
+  $scope.articleForm = 'templates/includes/article_form.html'
   $scope.error = {error: false,
                   msg: ""};
   $scope.success = {success: false,
                     msg: "Your article has been sucessfully submitted"}
 
-  $scope.article = {
+  $scope.tempArticle = {
     id: 0,
     url: "",
     title: "",
@@ -122,11 +95,11 @@ crossControllers.controller('NewArticleCtrl',['$scope','Article',function ($scop
   }
 
   $scope.articleSubmit = function () {
-    $scope.article.csrfmiddlewaretoken = $scope.csrftoken;
+    $scope.tempArticle.csrfmiddlewaretoken = $scope.csrftoken;
 
-    Article.save({articleID:'new'},$scope.article,function (value, response) {
+    Article.save({articleID:'new'},$scope.tempArticle,function (value, response) {
       $scope.articles.push(value);
-      $scope.article = {
+      $scope.tempArticle = {
         id: 0,
         url: "",
         title: "",
@@ -146,4 +119,51 @@ crossControllers.controller('NewArticleCtrl',['$scope','Article',function ($scop
 
 crossControllers.controller('SingleCrossCtrl',['$scope',function ($scope) {
   $scope.buttons = false;
+}]);
+
+crossControllers.controller('NewCrossCtrl',['$scope','Cross',function ($scope,Cross) {
+  $scope.newCross = {
+    company: "",
+    subcategory: "",
+    notes: "",
+    score: "",
+    article: $scope.article.id
+  }
+
+  function removeArticle (elements) {
+    var index = 0;
+    elements.some(function(article, i) {
+      if (article.id === $scope.article.id) {
+        index = i;
+        return true;
+      }
+    });
+    elements.splice(index,1);
+  }
+
+  $scope.CrossSubmit = function () {
+    $scope.newCross.csrfmiddlewaretoken = $scope.csrftoken;
+
+
+    Cross.save({crossID: 'new'},$scope.newCross,function (value,response) {
+      $scope.article.data = $scope.article.data || [];
+
+      // Replace element IDs with actual names
+      value.company = $.grep($scope.companies,function(v) {return v.id === value.company})[0].name;
+      value.subcategory = $.grep($scope.categories,function(v) {return v.id === value.subcategory})[0].name;
+
+      $scope.article.data.push(value);
+      $scope.analysis.analysis = false;
+      $scope.error.error = false;
+
+      //if the article is in the Unanalyzed list, move it to the analyzed list
+      if($scope.article.data.length === 1) {
+        $scope.crossList.push($scope.article);
+        removeArticle($scope.articles);
+      }
+    },function (response) {
+      $scope.error.msg = JSON.stringify(response.data);
+      $scope.error.error = true;
+    });   
+  }
 }]);
