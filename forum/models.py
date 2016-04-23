355a39b4c4945e5baa2e99a417e5cc6bfe810d78
Daiane Bushey <daianebushey@Daianes-MacBook.local>
2016-04-23 18:58:44 -0400

    Added author in model and migrated

--- forum/models.py
+++ forum/models.py
@@ -4,8 +4,10 @@ from django.conf import settings
 
 
 class Thread (models.Model):
-	#author = models.ForeignKey('auth.User')
+	author = models.ForeignKey(settings.AUTH_USER_MODEL)
 	subject = models.CharField(max_length=200)
+	created_date = models.DateTimeField(
+	        default=timezone.now)
 
 	def __str__(self):
 		return self.subject
