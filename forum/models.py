76cab175a6f66262d4c05c1eb60283ce6477782b
Daiane Bushey <daianebushey@Daianes-MacBook.local>
2016-04-23 19:53:55 -0400

    Added category model and migrated

--- forum/models.py
+++ forum/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
+class Category(models.Model):
+	author = models.ForeignKey(settings.AUTH_USER_MODEL)
+	name = models.CharField(max_length=200)
+	created_date = models.DateTimeField(
+	        default=timezone.now)
 
-class Thread (models.Model):
+	def __str__(self):
+		return self.name
+
+class Thread(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL)
 	subject = models.CharField(max_length=200)
 	created_date = models.DateTimeField(
 	        default=timezone.now)
+	category = models.ForeignKey(Category, default=4)
 
 	def __str__(self):
 		return self.subject
@@ -26,6 +35,8 @@ class Post(models.Model):
 
 	def __str__(self):
 	    return self.title
+
+
 
 
 
